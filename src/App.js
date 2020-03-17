5ecf1986e66ac509b011a4e2679b59ca2d61edf9
PaulsBecks <paul.beck.berlin@gmail.com>
2020-03-17 12:25:36 +0100

    some boilerplate code

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import { Index, Listings } from "./pages";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Header, Footer } from "./components";
 
+import "./App.css";
+
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Switch>
-        <Route exact path="/">
-          <Index />
-        </Route>
-        <Route path="/listings">
-          <Listings />
-        </Route>
-      </Switch>
+      <div className="csf-main-container">
+        <Switch>
+          <Route exact path="/">
+            <Index />
+          </Route>
+          <Route path="/listings">
+            <Listings />
+          </Route>
+        </Switch>
+      </div>
       <Footer />
     </BrowserRouter>
   );
--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,12 +1,74 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Header } from "../../components";
+import {
+  Container,
+  Button,
+  Header,
+  Segment,
+  Grid,
+  Divider,
+  Icon,
+  Message
+} from "semantic-ui-react";
 
 export default function() {
   return (
-    <div>
+    <Container>
       <h1>This is the index page</h1>
-      <Link to="/listings">Listings</Link>
-    </div>
+      <p>
+        Mit der Zeit steigen die Zahlen der Infizierten und Hilfsbedürftigen an.
+        Menschen, die nicht auf Familie oder Freunde zurückgreifen können,
+        insbesondere die, die zu einer Risikogruppe gehören, sind auf fremde
+        Hilfe angewiesen! Diese Seite erleichtert eine "erweiterte
+        Nachbarschaftshilfe" in Berlin. Teile diese Seite, um deinen Mitmenschen
+        zu helfen! #keinepanik #unleashhelp
+      </p>
+
+      <Segment placeholder>
+        <Grid columns={2} stackable textAlign="center">
+          <Divider vertical>Or</Divider>
+
+          <Grid.Row verticalAlign="middle">
+            <Grid.Column>
+              <Header icon>
+                <Icon name="search" />
+                Find a Helper
+              </Header>
+
+              <Button primary as={Link} to="/listings">
+                Get Help
+              </Button>
+            </Grid.Column>
+
+            <Grid.Column>
+              <Header icon>
+                <Icon name="world" />
+                Solidarité
+              </Header>
+              <Button primary as={Link}>
+                Offer Help
+              </Button>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+
+      <p>
+        Bei der Nutzung dieses Angebots handelst du eigenverantwortlich. Sei
+        bitte vorsichtig – Du stehst im Kontakt zu Fremden. Die Sicherheit,
+        Zuverlässigkeit und Qualität der Anbieter kann nicht garantiert werden.
+      </p>
+      <Message>
+        Um so viele Menschen wie möglich von der Idee, füreinander da zu sein,
+        anzustecken, suchen wir "erfolgreiche" Vermittlungen, bei denen dieses
+        Bürgerportal helfen konnte. Es muss nichts Spektakuläres sein, denn auch
+        kleine Dinge können eine große Hilfe sein. Da wir nicht nachverfolgen,
+        wer nun schlussendlich wem geholfen hat: meldet Euch gerne per Mail, da
+        es diesbezüglich Medienanfragen gibt.
+      </Message>
+      <div>
+        <Button big>Contact Us</Button>
+      </div>
+    </Container>
   );
 }
