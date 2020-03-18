2a95e629b76941bc7c6b55e4751cdbf1b6ec7d3c
PaulsBecks <paul.beck.berlin@gmail.com>
2020-03-18 09:43:00 +0100

    added custom domain

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,10 +5,11 @@ import { Icon } from "semantic-ui-react";
 
 export default function Header() {
   return (
-    <div class="csf-navigation">
-      <h1>
-        Corona Solidarité <Icon name="hand" />
-      </h1>
+    <div className="csf-navigation">
+      <div className="csf-navigation-text">
+        <h1>Corona Solidarité</h1>
+        <span>Help your neighbors</span>
+      </div>
     </div>
   );
 }
--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -11,63 +11,68 @@ import {
   Message
 } from "semantic-ui-react";
 
+import "./index.css";
+
 export default function() {
   return (
     <Container>
-      <h1>This is the index page</h1>
-      <p>
-        Mit der Zeit steigen die Zahlen der Infizierten und Hilfsbedürftigen an.
-        Menschen, die nicht auf Familie oder Freunde zurückgreifen können,
-        insbesondere die, die zu einer Risikogruppe gehören, sind auf fremde
-        Hilfe angewiesen! Diese Seite erleichtert eine "erweiterte
-        Nachbarschaftshilfe" in Berlin. Teile diese Seite, um deinen Mitmenschen
-        zu helfen! #keinepanik #unleashhelp
-      </p>
+      <div className="csf-index">
+        <h1>This is the index page</h1>
+        <p>
+          Mit der Zeit steigen die Zahlen der Infizierten und Hilfsbedürftigen
+          an. Menschen, die nicht auf Familie oder Freunde zurückgreifen können,
+          insbesondere die, die zu einer Risikogruppe gehören, sind auf fremde
+          Hilfe angewiesen! Diese Seite erleichtert eine "erweiterte
+          Nachbarschaftshilfe" in Berlin. Teile diese Seite, um deinen
+          Mitmenschen zu helfen! #keinepanik #unleashhelp
+        </p>
 
-      <Segment placeholder>
-        <Grid columns={2} stackable textAlign="center">
-          <Divider vertical>Or</Divider>
+        <Segment placeholder>
+          <Grid columns={2} stackable textAlign="center">
+            <Divider vertical>Or</Divider>
 
-          <Grid.Row verticalAlign="middle">
-            <Grid.Column>
-              <Header icon>
-                <Icon name="search" />
-                Find a Helper
-              </Header>
+            <Grid.Row verticalAlign="middle">
+              <Grid.Column>
+                <Header icon>
+                  <Icon name="search" />
+                  Find a Helper
+                </Header>
 
-              <Button primary as={Link} to="/listings">
-                Get Help
-              </Button>
-            </Grid.Column>
+                <Button primary as={Link} to="/listings">
+                  Get Help
+                </Button>
+              </Grid.Column>
 
-            <Grid.Column>
-              <Header icon>
-                <Icon name="world" />
-                Solidarité
-              </Header>
-              <Button primary as={Link}>
-                Offer Help
-              </Button>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
+              <Grid.Column>
+                <Header icon>
+                  <Icon name="world" />
+                  Solidarité
+                </Header>
+                <Button primary as={Link}>
+                  Offer Help
+                </Button>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
 
-      <p>
-        Bei der Nutzung dieses Angebots handelst du eigenverantwortlich. Sei
-        bitte vorsichtig – Du stehst im Kontakt zu Fremden. Die Sicherheit,
-        Zuverlässigkeit und Qualität der Anbieter kann nicht garantiert werden.
-      </p>
-      <Message>
-        Um so viele Menschen wie möglich von der Idee, füreinander da zu sein,
-        anzustecken, suchen wir "erfolgreiche" Vermittlungen, bei denen dieses
-        Bürgerportal helfen konnte. Es muss nichts Spektakuläres sein, denn auch
-        kleine Dinge können eine große Hilfe sein. Da wir nicht nachverfolgen,
-        wer nun schlussendlich wem geholfen hat: meldet Euch gerne per Mail, da
-        es diesbezüglich Medienanfragen gibt.
-      </Message>
-      <div>
-        <Button big>Contact Us</Button>
+        <p>
+          Bei der Nutzung dieses Angebots handelst du eigenverantwortlich. Sei
+          bitte vorsichtig – Du stehst im Kontakt zu Fremden. Die Sicherheit,
+          Zuverlässigkeit und Qualität der Anbieter kann nicht garantiert
+          werden.
+        </p>
+        <Message>
+          Um so viele Menschen wie möglich von der Idee, füreinander da zu sein,
+          anzustecken, suchen wir "erfolgreiche" Vermittlungen, bei denen dieses
+          Bürgerportal helfen konnte. Es muss nichts Spektakuläres sein, denn
+          auch kleine Dinge können eine große Hilfe sein. Da wir nicht
+          nachverfolgen, wer nun schlussendlich wem geholfen hat: meldet Euch
+          gerne per Mail, da es diesbezüglich Medienanfragen gibt.
+        </Message>
+        <div>
+          <Button big>Contact Us</Button>
+        </div>
       </div>
     </Container>
   );
