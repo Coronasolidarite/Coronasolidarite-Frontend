5c54fd9d441a979139a63d34b9c9e7185860ee5d
PaulsBecks <paul.beck.berlin@gmail.com>
2020-03-20 12:35:38 +0100

    added logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import "./index.css";
 import { Link } from "react-router-dom";
+import { Icon } from "semantic-ui-react";
 
 export default function Header() {
   return (
     <div className="csf-navigation">
       <div className="csf-navigation-text">
         <Link to="/" className="csf-header-title">
-          Corona Solidarité
+          Corona Solidarité <Icon name="heartbeat" />
         </Link>
         <span>Stay & Save</span>
       </div>
